6d63d61a259dcc297e5d74723f6ab2dfa6d0a9f6
ayush4828 <ayushpatel11486@gmail.com>
2026-06-01 21:16:50 +0530

    repo creationn feature added

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -5,7 +5,36 @@ const Issue = require("../model/issueModel");
 
 
 const createRepository = async(req,res)=>{
-    
+    const { owner, name, issues, content, description, visibility } = req.body;
+
+  try {
+    if (!name) {
+      return res.status(400).json({ error: "Repository name is required!" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(owner)) {
+      return res.status(400).json({ error: "Invalid User ID!" });
+    }
+
+    const newRepository = new Repository({
+      name,
+      description,
+      visibility,
+      owner,
+      content,
+      issues,
+    });
+
+    const result = await newRepository.save();
+
+    res.status(201).json({
+      message: "Repository created!",
+      repositoryID: result._id,
+    });
+  } catch (err) {
+    console.error("Error during repository creation : ", err.message);
+    res.status(500).send("Server error");
+  }
 }
 const getAllRepositories = async(req,res)=>{
     res.send("all repos fetched success!");

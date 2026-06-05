1c47923315ef1a82aa61e81d7e3f18b88079085c
ayush4828 <ayushpatel11486@gmail.com>
2026-06-05 21:39:44 +0530

    dashboard repo functionality added

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,53 @@
 const Dashboard = () => {
+
+  const [repositories, setRepositories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    const fetchRepositories = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3002/repo/user/${userId}`
+        );
+        const data = await response.json();
+        setRepositories(data.repositories);
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    const fetchSuggestedRepositories = async () => {
+      try {
+        const response = await fetch(`http://localhost:3002/repo/all`);
+        const data = await response.json();
+        setSuggestedRepositories(data);
+        console.log(suggestedRepositories);
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    fetchRepositories();
+    fetchSuggestedRepositories();
+  }, []);
+
+    useEffect(() => {
+    if (searchQuery == "") {
+      setSearchResults(repositories);
+    } else {
+      const filteredRepo = repositories.filter((repo) =>
+        repo.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setSearchResults(filteredRepo);
+    }
+  }, [searchQuery, repositories]);
+  
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Dashboard Page</h1>
-      <p>Dashboard component content here</p>
-    </div>
+      <h1>This is dashboard</h1>
   );
 };
 
